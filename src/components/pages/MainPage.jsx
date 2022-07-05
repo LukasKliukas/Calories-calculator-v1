8a48f5b472f6fd57042ddca91ac0ea5529938c93
Lukas Kliukas <lukaskliukas@gmail.com>
2022-07-05 21:06:34 +0300

    Real time calories counter

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 
 const MainPage = () => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [age, setAge] = useState('');
+
   var myData = {
-    weight: 112,
-    height: 189,
-    age: 29,
+    weight: weight,
+    height: height,
+    age: age,
   };
+
+  function submitHandler(event) {
+    event.preventDefault();
+  }
 
   function caloriesCounterBMR() {
     let totalCal = 0;
     totalCal = 10 * myData.weight + 6.25 * myData.height - 5 * myData.age + 5;
-    return totalCal;
+    return totalCal * 1.2;
   }
 
   let resultBMR = caloriesCounterBMR();
@@ -18,12 +27,31 @@ const MainPage = () => {
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <label htmlFor='weight'>Svoris</label>
-        <input type='number' id='weight' onChange={() => {}} />
+        <input
+          type='number'
+          id='weight'
+          onChange={(e) => setWeight(e.target.value)}
+          value={weight}
+        />
+        <label htmlFor='height'>Ūgis</label>
+        <input
+          type='number'
+          id='height'
+          onChange={(e) => setHeight(e.target.value)}
+          value={height}
+        />
+        <label htmlFor='age'>Amžius</label>
+        <input
+          type='number'
+          id='age'
+          onChange={(e) => setAge(e.target.value)}
+          value={age}
+        />
         <button>Submit</button>
       </form>
-      {resultBMR} Calories
+      {roundResult} Calories
     </div>
   );
 };
